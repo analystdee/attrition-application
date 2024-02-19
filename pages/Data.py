da7d18fb5cdc1f1ac25f6b64677cd3cc9d48bd50
khadija ahmed <deedahahmed01@gmail.com>
2024-02-19 18:31:36 +0300

    change page name

---
+++ pages/Data.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='View Data',
+    page_icon='',
+    layout='wide'
+)
+
+st.title(
+
+
+)
+
+def data_page():
+    st.title('About Page')
+    st.write('This is the about page of my Streamlit App.')
+    # Add more content as needed
