b90774780dba1bfbe3cc2647d9ce8396eceafc47
khadija ahmed <deedahahmed01@gmail.com>
2024-02-24 17:49:04 +0300

    update the config file,add the vodafone dataset

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
+from imaplib import _Authenticator
 import streamlit as st  
+import yaml
+from yaml.loader import SafeLoader
+import streamlit_authenticator as stauth
 
 def main():
-    st.title('Home Page')
-    st.write('Welcome to my attrition prediction App!')
-    st.write('This is a demo churning prediction application built with Streamlit.')
-    st.write('Please login to access the features.')
-    username = st.text_input('Username')
-    password = st.text_input('Password', type='password')
-    login_button = st.button('Login')
-
-    if login_button:
-        if username == 'Deedah' and password == 'password':
-            st.success('Logged in successfully!')
-            st.write('Welcome, {}'.format(username))
-        else:
-            st.error('Invalid username or password')
+        st.title('Home Page')
+        st.write('Welcome to my attrition prediction App!')
+        st.write('This is a demo churning prediction application built with Streamlit.')
+        st.write('Please login to access the features.')
+    
 
 
-    # Instructions
-    st.subheader('Instructions')
-    st.markdown("""
-    1. **Login**: Enter your username and password to login.
-    2. **Navigate**: After logging in, you can navigate to different pages using the sidebar.
-    3. **Explore**: Explore the features of each page and interact with the elements provided.
-    """)
+        with open('C:/Users/Said Ahmed/Desktop/personalprojects/attrition-application/config.yaml') as file:
+            config = yaml.load(file, Loader=SafeLoader)
 
-    # Feedback Mechanism
-    st.subheader('Feedback')
-    feedback = st.text_area('Please share your feedback or suggestions here:', height=100)
-    submit_button = st.button('Submit Feedback')
+        authenticator = stauth.Authenticate(
+            config['credentials'],
+            config['cookie']['name'],
+            config['cookie']['key'],
+            config['cookie']['expiry_days'],
+            config['preauthorized']
+            
+    )
+        authenticator.login()
 
-    if submit_button:
-        if feedback:
-            # Process the feedback (you can save it to a database, send it via email, etc.)
-            st.success('Thank you for your feedback! We appreciate your input.')
-        else:
-            st.warning('Please provide your feedback before submitting.')
+    
 
-    # Contact Information
-    st.subheader('Contact Us')
-    st.write('If you need assistance or have any questions, please contact us at deedahahmed01@gmail.com.')
 
+
+        # Feedback Mechanism
+        st.subheader('Feedback')
+        feedback = st.text_area('Please share your feedback or suggestions here:', height=100)
+        submit_button = st.button('Submit Feedback')
+
+        if submit_button:
+            if feedback:
+                # Process the feedback (you can save it to a database, send it via email, etc.)
+                st.success('Thank you for your feedback! We appreciate your input.')
+            else:
+                st.warning('Please provide your feedback before submitting.')
+
+        # Contact Information
+        st.subheader('Contact Us')
+        st.write('If you need assistance or have any questions, please contact us at deedahahmed01@gmail.com.')
 
 
 if __name__ == "__main__":
-    main()
+        main()
 
--- pages/1_Data.py
+++ pages/1_Data.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit as st
 import pandas as pd
 import mysql.connector
+import csv
 
 st.set_page_config(
     page_title='View Data',
@@ -10,54 +11,16 @@ st.set_page_config(
 )
 
 
-# Connect to MySQL database
-def connect_to_database():
-    try:
-        conn = mysql.connector.connect(
-         host='dap-projects-database.database.windows.net',
-         user='LP2_project',
-         password='Stat$AndD@t@Rul3',
-         database='dapDB'
-        )
-        return conn
-    except mysql.connector.Error as e:
-        st.error(f"Error connecting to MySQL database: {e}")
-        return None
 
-# Execute SQL query and return result as pandas DataFrame
-def execute_query(conn, query):
-    try:
-        cursor = conn.cursor()
-        cursor.execute(query)
-        result = cursor.fetchall()
-        columns = [i[0] for i in cursor.description]
-        df = pd.DataFrame(result, columns=columns)
-        cursor.close()
-        return df
-    except mysql.connector.Error as e:
-        st.error(f"Error executing SQL query: {e}")
-        return None
+# Define the path to your CSV file
+csv_file_path = r'C:\Users\Said Ahmed\Desktop\personalprojects\attrition-application\ml_dataset.csv'
 
-# Main function
-def main():
-    st.title("Streamlit App with MySQL Integration")
+# Initialize an empty list to store the data
+csv_data = []
 
-    # Connect to MySQL database
-    conn = connect_to_database()
-    if conn is None:
-        return
-
-    # Example SQL query
-    query = "SELECT * FROM your_table"
-
-    # Execute query and get data
-    data = execute_query(conn, query)
-
-    # Display data in Streamlit app
-    if data is not None:
-        st.write("Data from MySQL database:")
-        st.write(data)
-
-# Run the app
-if __name__ == "__main__":
-    main()
+# Reading data from the CSV file and storing it in the list
+with open(csv_file_path, 'r') as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        # Append each row as a dictionary to the csv_data list
+        csv_data.append(row)
