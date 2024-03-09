f53de7a636e99a473aa1f5227c4f54122a0ee6e1
khadija ahmed <deedahahmed01@gmail.com>
2024-03-09 23:18:22 +0300

    add login section on the sidebar in the main page

--- main.py
+++ main.py
@@ -12,29 +12,40 @@ st.set_page_config(
 )
 
 def main():
+    # Initialize session state
+    if 'authenticated' not in st.session_state:
+        st.session_state.authenticated = False
+
+    if not st.session_state.authenticated:
+        with st.sidebar:
+            st.title('Login')
+    
+            with open('C:/Users/Said Ahmed/Desktop/personalprojects/attrition-application/config.yaml') as file:
+                config = yaml.load(file, Loader=SafeLoader)
+
+            authenticator = stauth.Authenticate(
+                config['credentials'],
+                config['cookie']['name'],
+                config['cookie']['key'],
+                config['cookie']['expiry_days'],
+                config['preauthorized']           
+    )
+            authenticator.login()
+            st.session_state.authenticated = True
+
+    else:
         st.title('Home Page')
         st.write('Welcome to my attrition prediction App!')
         st.write('This is a demo churning prediction application built with Streamlit.')
-        st.write('Please login to access the features.')
-    
+        st.write('Please login to access the features. user: Khadija ,  password: 123')
 
-
-        with open('C:/Users/Said Ahmed/Desktop/personalprojects/attrition-application/config.yaml') as file:
-            config = yaml.load(file, Loader=SafeLoader)
-
-        authenticator = stauth.Authenticate(
-            config['credentials'],
-            config['cookie']['name'],
-            config['cookie']['key'],
-            config['cookie']['expiry_days'],
-            config['preauthorized']
-            
-    )
-        authenticator.login()
+        # Add sections explaining how the app works
+        st.header('How the App Works')
+        st.write('This app predicts churn based on a machine learning model trained on historical data.')
+        st.write('To make a prediction, users need to provide certain input features such as customer information and service usage.')
+        st.write('The model then processes this information and outputs the likelihood of a customer churning.')
 
     
-
-
 
         # Feedback Mechanism
         st.subheader('Feedback')
